422123714a2965c561941fd31b8a82cb3f6f28ef
khale <khaledboussaba@gmail.com>
2020-03-23 14:18:59 +0100

    create entity, service and rest controller for class produit

---
+++ src/main/java/com/project/stock/service/IProduitService.java
@@ -0,0 +1,17 @@
+package com.project.stock.service;
+
+import java.util.List;
+
+import com.project.stock.entity.Produit;
+
+public interface IProduitService {
+
+	List<Produit> getProduits();
+	
+	void addProduit(Produit produit);
+	
+	void updateProduit(Produit produit);
+	
+	void deleteProduit(String reference);
+	
+}
