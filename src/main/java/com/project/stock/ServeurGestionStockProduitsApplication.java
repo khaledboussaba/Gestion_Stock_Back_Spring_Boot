5043e9859c4f9870da5aab275718f20620f84444
khale <khaledboussaba@gmail.com>
2020-03-24 16:42:46 +0100

    creation of users and roles for connection to the website

--- src/main/java/com/project/stock/ServeurGestionStockProduitsApplication.java
+++ src/main/java/com/project/stock/ServeurGestionStockProduitsApplication.java
@@ -1,23 +1,51 @@
 package com.project.stock;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.project.stock.entity.Produit;
+import com.project.stock.entity.Role;
+import com.project.stock.entity.User;
+import com.project.stock.repository.ProduitRepository;
+import com.project.stock.repository.RoleRepository;
+import com.project.stock.repository.UserRepository;
 
 @SpringBootApplication
 public class ServeurGestionStockProduitsApplication {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+//		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
 		
-//		ConfigurableApplicationContext ctx =
-//				SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+		ConfigurableApplicationContext ctx =
+				SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
 		
-//		ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
-//		
-//		produitRepository.save(new Produit("Livre", 50, 20));
-//		produitRepository.save(new Produit("Cahier", 200, 5.25f));
-//		produitRepository.save(new Produit("Stylo", 500, 2.10f));
+		ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
+		
+		produitRepository.save(new Produit("Livre", 50, 20));
+		produitRepository.save(new Produit("Cahier", 200, 5.25f));
+		produitRepository.save(new Produit("Stylo", 500, 2.10f));
+		
+		RoleRepository roleRepository = ctx.getBean(RoleRepository.class);
+		
+		Role roleUser = new Role("ROLE_USER");
+		roleRepository.save(roleUser);
+		
+		Role roleAdmin = new Role("ROLE_ADMIN");
+		roleRepository.save(roleAdmin);
+		
+		UserRepository userRepository = ctx.getBean(UserRepository.class);
+		
+		User user = new User("user", "pwd", true);
+		user.setRoles(Arrays.asList(roleUser));
+		userRepository.save(user);
+
+		User admin = new User("admin", "pwd1", true);
+		admin.setRoles(Arrays.asList(roleUser, roleAdmin));
+		userRepository.save(admin);
 				
 	}
 
